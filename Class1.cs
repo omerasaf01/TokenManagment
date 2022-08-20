be835a53e6062d95c0655be0707b03631e8b29f5
Ömer Asaf Karasu <omerconsept999@gmail.com>
2022-08-20 21:44:08 +0300

    Version 1.0.0

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+
+namespace NiafixAuthentication
+{
+    public class ExampleModel
+    {
+        public long ID { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public long Permission { get; set; }
+    }
+    public class TokenManagment
+    {
+        public static List<Claim> TokenDecoder(string Token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            Token = Token.ToString();
+            var token = Token.Split(" ");
+
+            var EncodedToken = handler.ReadJwtToken(token[1]);
+
+            return EncodedToken.Claims.ToList();
+        }
+        
+        public static string CreateToken(ExampleModel _model)
+        {
+            var bytes = Encoding.UTF8.GetBytes("7A24432646294A404E635266546A576E5A7234753778214125442A472D4B6150");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials cred  = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            
+            var Claims = new Claim[] 
+            {
+                new Claim("ID", _model.ID.ToString()),
+                new Claim("Username", _model.Username),
+                new Claim("Name", _model.Name),
+                new Claim("Permission", _model.Permission.ToString())
+            };
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                expires: DateTime.UtcNow.AddDays(1),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: cred,
+                claims: Claims
+            );
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # TokenManagment
 .Net Core 6.0 TokenManagment Library
+
+## Documantation
+
+### Using
+
+```csharp
+...
+using NiafixAuthentication;
+
+namespace Example.Controllers
+{
+    [ApiController]
+    [Route("/api/authentication")]
+    public class ExampleController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult login(ExampleModel, UserData)
+        {
+            //if user exist
+            return Ok(TokenManagment.CreateToken(UserData));
+        }
+
+        [HttpGet]
+        [Authorization]
+        public IActionResult ControlToken()
+        {
+            return Ok(TokenManagment.TokenDecoder(Request.Headers["Authorization"]));
+
+        }
+    }
+}
+```
